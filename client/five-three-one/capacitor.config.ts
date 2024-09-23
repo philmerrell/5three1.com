3a5d7d4a494750a1b9cff77e621cba47e7c41502
Phil Merrell <philmerrell@boisestate.edu>
2024-09-23 13:54:25 -0600

    chore: first commit

---
+++ client/five-three-one/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Five Three One',
+  webDir: 'www'
+};
+
+export default config;
